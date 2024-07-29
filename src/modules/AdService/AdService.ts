b39828f130ffa343646a14e1a7ef006ca7eae668
Gaina Dan-Lucian <dan.gaina@connatix.com>
2024-07-29 17:11:26 +0300

    [feat] add message for ad blockers

--- src/modules/AdService/AdService.ts
+++ src/modules/AdService/AdService.ts
@@ -6,12 +6,21 @@ export class AdService {
     private adUrls = [VPAID_AD_URL, VIDEO_AD_URL, IMA_AD_URL];
 
     public static async requestAdByUrl(url: string): Promise<string> {
-        const response = await fetch(url, {
-            method: 'GET'
-        });
+        let response;
+        try {
+            response = await fetch(url, {
+                method: 'GET'
+            });
+        } catch (error) {
+            // eslint-disable-next-line no-alert
+            alert('Ad request failed, disable ad blockers and try again');
+        }
+
+        if (!response) {
+            throw new Error('Empty response');
+        }
 
         const decodedResponse = await response.text();
-
         if (!response.ok) {
             throw new Error(decodedResponse);
         }
